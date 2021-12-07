cbd5da707108a1f8972b51818a95694bf6d4ac15
riccardocaranfil <riccardo.caranfil@prontopro.it>
2021-12-07 20:59:25 +0100

    fix: tailwind purge files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  purge: ["./public/**/*.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  purge: [
+    "./pages/**/*.js",
+    "./pages/**/*.ts",
+    "./pages/**/*.jsx",
+    "./pages/**/*.tsx",
+    "./components/**/*.js",
+    "./components/**/*.ts",
+    "./components/**/*.jsx",
+    "./components/**/*.tsx",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
