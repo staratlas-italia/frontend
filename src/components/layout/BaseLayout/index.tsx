2fd545973f1c924df28b6ee5c069ea5c5c3a2211
riccardocaranfil <riccardo.caranfil@prontopro.it>
2021-12-11 09:57:20 +0100

    fix: ios background issue

--- src/components/layout/BaseLayout/index.tsx
+++ src/components/layout/BaseLayout/index.tsx
@@ -2,8 +2,8 @@ import React, { PropsWithChildren } from "react";
 import styled from "styled-components";
 import { Header } from "~/components/layout/Header";
 
-const BaseLayoutWrapper = styled.div.attrs({
-  className: "bg-fixed bg-no-repeat bg-cover bg-center min-h-screen	w-screen",
+const LayoutBackground = styled.div.attrs({
+  className: "fixed bg-no-repeat bg-cover bg-center min-h-screen	w-screen",
 })`
   background-image: url("/images/bg.webp");
 `;
@@ -11,12 +11,13 @@ const BaseLayoutWrapper = styled.div.attrs({
 export const BaseLayout = React.memo(
   ({ children }: PropsWithChildren<unknown>) => {
     return (
-      <BaseLayoutWrapper>
+      <div>
+        <LayoutBackground />
         <Header />
         <div className="z-30 container overflow-auto mx-auto py-10 px-5">
           {children}
         </div>
-      </BaseLayoutWrapper>
+      </div>
     );
   }
 );
