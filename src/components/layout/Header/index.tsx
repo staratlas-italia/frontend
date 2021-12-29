177f3a82f9d31228206f1026863453490c216ce2
riccardocaranfil <riccardo.caranfil@prontopro.it>
2021-12-29 17:32:37 +0100

    feat: add locale in routes

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
+import { Text } from "~/components/common/Text";
 import { Flex } from "~/components/layout/Flex";
 import { AtlasUsdcChange } from "~/components/layout/Header/components/AtlasUsdcChange";
 import { Treasury } from "~/components/layout/Header/components/Treasury";
 import { Wallet } from "~/components/Wallet";
 
 export const LogoLink = () => {
+  const { locale } = useRouter();
   return (
-    <Link href={`/`}>
+    <Link href={`/`} locale={locale}>
       <span className="cursor-pointer">
         <Flex align="center">
           <Image
@@ -24,18 +27,33 @@ export const LogoLink = () => {
   );
 };
 
-export const Header = () => (
-  <Flex align="center" grow={1} py={4} px={10} justify="center">
-    <Flex className="container" justify="between">
-      <LogoLink />
+export const Header = () => {
+  const { locale, asPath } = useRouter();
+  return (
+    <Flex align="center" grow={1} py={4} px={10} justify="center">
+      <Flex className="container" justify="between">
+        <LogoLink />
 
-      <Flex className="hidden">
-        <Wallet />
-      </Flex>
-      <Flex className="z-40 space-x-7">
-        <AtlasUsdcChange />
-        <Treasury />
+        <Flex className="hidden">
+          <Wallet />
+        </Flex>
+        <Flex className="z-40 space-x-7">
+          <AtlasUsdcChange />
+          <Treasury />
+
+          <Link href={asPath} locale={locale === "it" ? "en" : "it"}>
+            <Text
+              as="a"
+              color="white"
+              size="5xl"
+              transform="uppercase"
+              weight="semibold"
+            >
+              {locale}
+            </Text>
+          </Link>
+        </Flex>
       </Flex>
     </Flex>
-  </Flex>
-);
+  );
+};
