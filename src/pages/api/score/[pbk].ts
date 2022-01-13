7cbef77c93495c04636444132317d6fe52babbfa
lorenzodelia125 <lorenzodelia125@gmail.com>
2022-01-13 11:54:29 +0100

    feat: ship api cost and reward calc

--- src/pages/api/score/[pbk].ts
+++ src/pages/api/score/[pbk].ts
@@ -5,6 +5,7 @@ import { SA_FLEET_PROGRAM_ID } from "~/common/constants";
 import { ScoreFleetResponse } from "~/types/api";
 import { getConnectionContext } from "~/utils/connection";
 import { isPublicKey } from "~/utils/pubkey";
+import { getScoreVarsShipInfo } from "@staratlas/factory";
 
 const connection = new Connection(
   getConnectionContext("mainnet-beta").endpoint
@@ -32,9 +33,62 @@ export default async (
     new PublicKey(SA_FLEET_PROGRAM_ID)
   );
 
+  const shipsVars = await Promise.all(
+    accounts.map(async (account) => {
+      return getScoreVarsShipInfo(
+        connection,
+        new PublicKey(SA_FLEET_PROGRAM_ID),
+        new PublicKey(account.shipMint.toString())
+      );
+    })
+  );
+
+  const accountsWithVars = accounts.map((item, i) =>
+    Object.assign(item, shipsVars[i])
+  );
+
+  const FUEL_PRICE: number = 0.0014336;
+  const FOOD_PRICE: number = 0.0006144;
+  const ARMS_PRICE: number = 0.0021504;
+  const TOOLKIT_PRICE: number = 0.0017408;
+
+  const resDailyConsumption = (burnRate: number) => {
+    return Math.round(86400000 / burnRate);
+  };
+
+  const resDailyCostInAtlas = (resource: number, burnRate: number) => {
+    return Math.round((86400000 / burnRate) * resource * 100000000) / 100000000;
+  };
+
+  const dailyMaintenanceCostInAtlas = (
+    fuel: number,
+    food: number,
+    arms: number,
+    toolkit: number
+  ) => {
+    return Math.round((fuel + food + arms + toolkit) * 100000000) / 100000000;
+  };
+
+  const grossDailyRewardInAtlas = (rewardRatePerSecond: number) => {
+    return (
+      Math.round(rewardRatePerSecond * 0.00000001 * 60 * 60 * 24 * 100000000) /
+      100000000
+    );
+  };
+
+  const netDailyRewardInAtlas = (
+    rewardInAtlas: number,
+    maintenanceCostInAtlas: number
+  ) => {
+    return (
+      Math.round((rewardInAtlas - maintenanceCostInAtlas) * 100000000) /
+      100000000
+    );
+  };
+
   res.status(200).json({
     success: true,
-    data: accounts.map((account) => ({
+    data: accountsWithVars.map((account) => ({
       owner: account.owner.toString(),
       factionId: account.factionId.toString(),
       shipMint: account.shipMint.toString(),
@@ -56,6 +110,66 @@ export default async (
       stakedTimePaid: account.stakedTimePaid.toNumber(),
       pendingRewards: account.pendingRewards.toNumber(),
       totalRewardsPaid: account.totalRewardsPaid.toNumber(),
+      /* Vars */
+      rewardRatePerSecond: account.rewardRatePerSecond.toNumber(),
+      fuelMaxReserve: account.fuelMaxReserve,
+      foodMaxReserve: account.foodMaxReserve,
+      armsMaxReserve: account.armsMaxReserve,
+      toolkitMaxReserve: account.toolkitMaxReserve,
+      millisecondsToBurnOneFuel: account.millisecondsToBurnOneFuel,
+      millisecondsToBurnOneFood: account.millisecondsToBurnOneFood,
+      millisecondsToBurnOneArms: account.millisecondsToBurnOneArms,
+      millisecondsToBurnOneToolkit: account.millisecondsToBurnOneToolkit,
+      /* Custom values */
+      dailyFuelConsumption: resDailyConsumption(
+        account.millisecondsToBurnOneFuel
+      ),
+      dailyFoodConsumption: resDailyConsumption(
+        account.millisecondsToBurnOneFood
+      ),
+      dailyArmsConsumption: resDailyConsumption(
+        account.millisecondsToBurnOneArms
+      ),
+      dailyToolkitConsumption: resDailyConsumption(
+        account.millisecondsToBurnOneToolkit
+      ),
+      dailyFuelCostInAtlas: resDailyCostInAtlas(
+        FUEL_PRICE,
+        account.millisecondsToBurnOneFuel
+      ),
+      dailyFoodCostInAtlas: resDailyCostInAtlas(
+        FOOD_PRICE,
+        account.millisecondsToBurnOneFood
+      ),
+      dailyArmsCostInAtlas: resDailyCostInAtlas(
+        ARMS_PRICE,
+        account.millisecondsToBurnOneArms
+      ),
+      dailyToolkitCostInAtlas: resDailyCostInAtlas(
+        TOOLKIT_PRICE,
+        account.millisecondsToBurnOneToolkit
+      ),
+      dailyMaintenanceCostInAtlas: dailyMaintenanceCostInAtlas(
+        resDailyCostInAtlas(FUEL_PRICE, account.millisecondsToBurnOneFuel),
+        resDailyCostInAtlas(FOOD_PRICE, account.millisecondsToBurnOneFood),
+        resDailyCostInAtlas(ARMS_PRICE, account.millisecondsToBurnOneArms),
+        resDailyCostInAtlas(TOOLKIT_PRICE, account.millisecondsToBurnOneToolkit)
+      ),
+      grossDailyRewardInAtlas: grossDailyRewardInAtlas(
+        account.rewardRatePerSecond.toNumber()
+      ),
+      netDailyRewardInAtlas: netDailyRewardInAtlas(
+        grossDailyRewardInAtlas(account.rewardRatePerSecond.toNumber()),
+        dailyMaintenanceCostInAtlas(
+          resDailyCostInAtlas(FUEL_PRICE, account.millisecondsToBurnOneFuel),
+          resDailyCostInAtlas(FOOD_PRICE, account.millisecondsToBurnOneFood),
+          resDailyCostInAtlas(ARMS_PRICE, account.millisecondsToBurnOneArms),
+          resDailyCostInAtlas(
+            TOOLKIT_PRICE,
+            account.millisecondsToBurnOneToolkit
+          )
+        )
+      ),
     })),
   });
 };
