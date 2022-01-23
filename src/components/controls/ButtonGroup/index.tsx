abb0e8ee92ca14e4cb1d66de58a6b18caa1a7331
emberist <13602797+emberist@users.noreply.github.com>
2022-01-23 01:01:33 +0100

    feat: improve table component

--- src/components/controls/ButtonGroup/index.tsx
+++ src/components/controls/ButtonGroup/index.tsx
@@ -1,4 +1,5 @@
 import classNames from "classnames";
+import { Text } from "~/components/common/Text";
 import { Flex } from "~/components/layout/Flex";
 
 type Props = {
@@ -9,22 +10,29 @@ type Props = {
 
 export const ButtonGroup = ({ items, onAction, selectedItem }: Props) => {
   return (
-    <Flex className="rounded-md shadow-sm" role="group">
-      {items.map(([key, value], index) => (
-        <button
-          key={key}
-          type="button"
-          onClick={() => onAction(key)}
-          className={classNames(
-            "py-2 px-4 text-sm font-medium text-gray-900 bg-white  border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white",
-            { "bg-gray-200": selectedItem === key },
-            { "rounded-l-lg": index === 0 },
-            { "rounded-r-lg": index === items.length - 1 }
-          )}
-        >
-          {value}
-        </button>
-      ))}
+    <Flex>
+      <Flex
+        className="rounded-xl shadow-sm space-x-2 bg-opacity-20"
+        color="white"
+        px={3}
+        py={2}
+      >
+        {items.map(([key, value]) => (
+          <button
+            key={key}
+            type="button"
+            onClick={() => onAction(key)}
+            className={classNames(
+              "py-2 px-3 text-sm rounded-xltext-gray-90 rounded-xl hover:bg-gray-200",
+              { "bg-white bg-opacity-90 ": selectedItem === key }
+            )}
+          >
+            <Text weight={selectedItem === key ? "bold" : "semibold"}>
+              {value}
+            </Text>
+          </button>
+        ))}
+      </Flex>
     </Flex>
   );
 };
--- src/views/Ships/components/ShipTable/index.tsx
+++ src/views/Ships/components/ShipTable/index.tsx
@@ -1,109 +1,34 @@
-import { ExternalLinkIcon } from "@heroicons/react/solid";
-import Link from "next/link";
 import { useRouter } from "next/router";
-import { useCallback, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import { useIntl } from "react-intl";
 import { ButtonGroup } from "~/components/controls/ButtonGroup";
 import { BlurBackground } from "~/components/layout/BlurBackground";
 import { Flex } from "~/components/layout/Flex";
 import { Table } from "~/components/Table";
-import { ShipSize, shipSizes } from "~/types";
-import { fillUrlParameters } from "~/utils/fillUrlParameters";
-import { getRoute } from "~/utils/getRoute";
-import { useShipsTable } from "~/views/Ships/components/ShipTable/useShipsTable";
-import { buildDiscountColumn } from "~/views/Ships/components/ShipTable/utils/buildDiscountColumn";
-import { buildNameColumn } from "~/views/Ships/components/ShipTable/utils/buildNameColumn";
+import { columns } from "~/views/Ships/components/ShipTable/columns";
 import {
-  buildAtlasPriceColumn,
-  buildPriceColumn,
-} from "~/views/Ships/components/ShipTable/utils/buildPriceColumn";
+  MarketAction,
+  useShipsTable,
+} from "~/views/Ships/components/ShipTable/useShipsTable";
 
 export const ShipTable = () => {
-  const [size, setSize] = useState<ShipSize>("medium");
+  const [action, setAction] = useState<MarketAction>("buy");
 
-  const [fetch, { data, atlasPrice, loading }] = useShipsTable(size);
+  const [fetch, { data, atlasPrice }] = useShipsTable();
 
   const intl = useIntl();
 
   const { locale } = useRouter();
 
-  const fetchData = useCallback(() => {
-    fetch();
-  }, [fetch]);
-
   const cols = useMemo(
-    () => [
-      buildNameColumn({
-        name: intl.formatMessage({
-          id: "Ships.Table.Column.name",
-          defaultMessage: "Nome",
-        }),
-        accessor: "name",
-        imageUrlAccessor: "imageUrl",
+    () =>
+      columns({
+        action,
+        locale,
+        atlasPrice,
+        formatMessage: intl.formatMessage,
       }),
-      buildPriceColumn({
-        name: intl.formatMessage({
-          id: "Ships.Table.Column.price",
-          defaultMessage: "Prezzo USDC",
-        }),
-        accessor: "price",
-        currency: "USDC",
-      }),
-      buildAtlasPriceColumn({
-        name: intl.formatMessage({
-          id: "Ships.Table.Column.atlasPrice",
-          defaultMessage: "Prezzo Atlas",
-        }),
-        accessor: "atlasPrice",
-        atlasValue: atlasPrice,
-      }),
-      buildPriceColumn({
-        name: intl.formatMessage({
-          id: "Ships.Table.Column.vwap",
-          defaultMessage: "VWAP",
-        }),
-        accessor: "vwapPrice",
-        currency: "USDC",
-      }),
-      buildDiscountColumn({
-        name: intl.formatMessage({
-          id: "Ships.Table.Column.priceVsVwapPrice",
-          defaultMessage: "Prezzo vs VWAP",
-        }),
-        accessor: "priceVsVwapPrice",
-        suffix: " %",
-      }),
-      buildDiscountColumn({
-        name: intl.formatMessage({
-          id: "Ships.Table.Column.atlasPriceVsVwapPrice",
-          defaultMessage: "Prezzo Atlas vs VWAP",
-        }),
-        accessor: "atlasPriceVsVwapPrice",
-        suffix: " %",
-      }),
-      {
-        Header: "",
-        id: "actions",
-        sortDisabled: true,
-        Cell: ({ row }) => {
-          return (
-            <Flex px={3}>
-              <Link
-                href={fillUrlParameters(getRoute("/ships/:shipId"), {
-                  shipId: row.original.id,
-                })}
-                locale={locale}
-              >
-                <a target="_blank">
-                  <ExternalLinkIcon className="h-5 w-5" />
-                </a>
-              </Link>
-            </Flex>
-          );
-        },
-      },
-    ],
-    [atlasPrice, intl]
+    [action, atlasPrice, intl]
   );
 
   return (
@@ -114,15 +39,18 @@ export const ShipTable = () => {
         justify="center"
       >
         <ButtonGroup
-          items={shipSizes.map((size) => [size, size])}
-          onAction={(size) => setSize(size as ShipSize)}
-          selectedItem={size}
+          items={[
+            ["buy", "I want to buy"],
+            ["sell", "I want to sell"],
+          ]}
+          onAction={(action) => setAction(action as MarketAction)}
+          selectedItem={action}
         />
         <Table
           columns={cols}
           data={data}
-          fetchData={fetchData}
-          loading={loading}
+          fetchData={fetch}
+          //loading={loading}
         />
       </Flex>
     </BlurBackground>
