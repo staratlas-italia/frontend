f824454758d4e13fae5db693287e03ba10493c6c
lorenzodelia125 <lorenzodelia125@gmail.com>
2022-01-24 01:49:15 +0100

    fix: urgent - resources and earnings calc updated

--- src/utils/grossDailyRewardInAtlas/index.ts
+++ src/utils/grossDailyRewardInAtlas/index.ts
@@ -1,9 +1,14 @@
 import { ATLAS_DECIMAL } from "~/common/constants";
 
-export const grossDailyRewardInAtlas = (rewardRatePerSecond: number) => {
+export const grossDailyRewardInAtlas = (
+  rewardRatePerSecond: number,
+  shipQuantityInEscrow: number
+) => {
   return (
-    Math.round(
+    (Math.round(
       (rewardRatePerSecond / ATLAS_DECIMAL) * 60 * 60 * 24 * ATLAS_DECIMAL
-    ) / ATLAS_DECIMAL
+    ) /
+      ATLAS_DECIMAL) *
+    shipQuantityInEscrow
   );
 };
