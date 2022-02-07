598e61adcd8020ddc5435ed335a93c6f6ce24939
emberist <13602797+emberist@users.noreply.github.com>
2022-02-07 19:55:16 +0100

    feat: add apple touch icon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,14 +5,12 @@ export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
-
     try {
       ctx.renderPage = () =>
         originalRenderPage({
           enhanceApp: (App) => (props) =>
             sheet.collectStyles(<App {...props} />),
         });
-
       const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
@@ -20,6 +18,16 @@ export default class MyDocument extends Document {
           <>
             {initialProps.styles}
             {sheet.getStyleElement()}
+            <link rel="apple-touch-icon" href="/images/apple-touch-icon.png" />
+            <meta name="apple-mobile-web-app-capable" content="yes"></meta>
+            <meta
+              name="apple-mobile-web-app-status-bar-style"
+              content="black"
+            ></meta>
+            <meta
+              name="apple-mobile-web-app-title"
+              content="StarAtlasItalia"
+            ></meta>
           </>
         ),
       };
