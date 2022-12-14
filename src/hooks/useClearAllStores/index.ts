fb6b99612ce91e31736d58bc9a159ca37dd69cae
emberist <13602797+emberist@users.noreply.github.com>
2022-12-14 14:54:07 +0100

    feat: remove lastConnectedWallet property

--- src/hooks/useClearAllStores/index.ts
+++ src/hooks/useClearAllStores/index.ts
@@ -1,5 +1,4 @@
 import { useCallback } from "react";
-import { useAppStore } from "~/stores/useAppStore";
 import { useAuthStore } from "~/stores/useAuthStore";
 import { useBadgesStore } from "~/stores/useBadgesStore";
 import { useFleetStore } from "~/stores/useFleetStore";
@@ -7,7 +6,6 @@ import { usePlayerStore } from "~/stores/usePlayerStore";
 
 export const useClearAllStores = () =>
   useCallback(() => {
-    useAppStore.setState({ lastConnectedWallet: null });
     useAuthStore.getState().clear();
     useFleetStore.getState().clear();
     useBadgesStore.getState().clear();
--- src/stores/useAppStore/index.ts
+++ src/stores/useAppStore/index.ts
@@ -3,14 +3,12 @@ import { persist } from "zustand/middleware";
 
 type AppStore = {
   showHueAnimation: boolean;
-  lastConnectedWallet: string | null;
 };
 
 export const useAppStore = create(
   persist<AppStore>(
     () => ({
       showHueAnimation: true,
-      lastConnectedWallet: null,
     }),
     {
       name: "app-storage",
