ba3e39751674bec0839cac440a016f5ee90bad0f
emberist <13602797+emberist@users.noreply.github.com>
2022-12-19 21:09:38 +0100

    fix: signature system

--- src/pages/admin/index.page.tsx
+++ src/pages/admin/index.page.tsx
@@ -1,12 +1,8 @@
 import { AssertAuthenticated } from "~/components/auth/AssertAuthenticated";
-import { LoadingView } from "~/components/LoadingView";
 import { View } from "./View";
 
 const AdminPage = () => (
-  <AssertAuthenticated
-    adminOnly
-    loader={<LoadingView title="Admin.SignatureLoader.title" />}
-  >
+  <AssertAuthenticated adminOnly>
     <View />
   </AssertAuthenticated>
 );
